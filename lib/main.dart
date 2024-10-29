f629a5b66544786d807ad26596cdf0913b926ebc
RahmatulFirdaus <rahmatulfirdausuniska@gmail.com>
2024-10-29 17:28:32 +0800

    another fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:id_camp_final_project/pages/get_x_controller.dart';
-import 'package:id_camp_final_project/pages/intro_screen.dart';
-import 'package:id_camp_final_project/pages/login_page/sign_in.dart';
 import 'package:id_camp_final_project/splash_screen/opening.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
